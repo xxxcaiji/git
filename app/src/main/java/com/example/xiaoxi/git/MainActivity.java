23b454bb52329c588ab54a6af4520e1803f66909
xxxcaiji <38600682+xxxcaiji@users.noreply.github.com>
2018-08-15 17:53:17 +0800

    第三次

--- app/src/main/java/com/example/xiaoxi/git/MainActivity.java
+++ app/src/main/java/com/example/xiaoxi/git/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("tags", "第二次提交");
+        Log.i("tags", "第二次提交");
     }
 }
