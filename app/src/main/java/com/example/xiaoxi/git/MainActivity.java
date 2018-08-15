0a0e32a6937332db7bd080df02491c1213a7500f
xxxcaiji <38600682+xxxcaiji@users.noreply.github.com>
2018-08-15 17:43:13 +0800

    第二次

--- app/src/main/java/com/example/xiaoxi/git/MainActivity.java
+++ app/src/main/java/com/example/xiaoxi/git/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.xiaoxi.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("tags", "tags");
     }
 }
